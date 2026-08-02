2733a98a3bdf712cdd916bc6f5c12bcddeef1316
niteshkumar-11n <nitteshkumar12570@gmail.com>
2026-08-02 17:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.72%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,13 +1,19 @@
 class Solution {
-    public int  winner(int[] nums,int i,int j) {
+    public int  winner(int[] nums,int i,int j,int [][] dp) {
         if(i>j) return 0;
         if(i == j) return nums[i];
-        int take_i = nums[i] - winner(nums, i+1, j);
-        int take_j = nums[j] - winner(nums, i, j-1);
-        return Math.max( take_i , take_j);
+        if(dp[i][j] !=-1) return dp[i][j];
+        int take_i = nums[i] - winner(nums, i+1, j,dp);
+        int take_j = nums[j] - winner(nums, i, j-1,dp);
+        
+        return dp[i][j] = Math.max( take_i , take_j);
     }
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return winner(nums,0,n-1)>=0;
+        int [][] dp = new int [n][n];
+        for(int [] row:dp) {
+            Arrays.fill(row,-1);
+        }
+        return winner(nums,0,n-1,dp)>=0;
     }
 }
