038bc2681d7a4645c1e635d0b2671c8fda5a9091
niteshkumar-11n <nitteshkumar12570@gmail.com>
2026-08-05 23:22:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (58.38%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy  = new ListNode(-1);
+        ListNode temp = dummy;
+        int carry = 0;
+        while(l1 !=null || l2 != null){
+            int val1 =0,val2 = 0;
+            if(l1 != null) val1 = l1.val;
+            if(l2 != null) val2 = l2.val;
+            int num =  val1+val2+carry;
+            ListNode t = new ListNode(num%10);
+            temp.next = t;
+            temp = temp.next;
+            if(num>9) carry =1;
+            else carry =0;
+            if(l1 != null)  l1 = l1.next;
+            if(l2 != null)  l2 = l2.next;
+        }
+        if(carry ==1){
+            ListNode t = new ListNode(1);
+            temp.next = t;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
