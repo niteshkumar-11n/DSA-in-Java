31bbb575d455b584d627e869da0f5358d4e898f6
niteshkumar-11n <nitteshkumar12570@gmail.com>
2026-09-07 17:27:10 +0530

    Time: 7 ms (42.98%) | Memory: 46.8 MB (16.77%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,57 @@
+class Solution {
+    public class Pair {
+        int node;
+        int  cost;
+
+        Pair(int node , int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+    public class Triplate implements Comparable<Triplate>{
+        int node ;
+        int cost;
+        int stops;
+        Triplate(int node, int cost, int stops){
+            this.stops =  stops;
+            this.node = node;
+            this.cost = cost;
+        }
+        public int compareTo(Triplate t){
+            if(t.stops == this.stops) return Integer.compare(this.node, t.node);
+            return Integer.compare(this.stops, t.stops);
+
+        }
+
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)  adj.add(new ArrayList<Pair>());
+        for (int i = 0; i <flights.length; i++){
+            int from = flights[i][0], to = flights[i][1] , cost = flights[i][2];
+            adj.get(from).add(new Pair(to,cost));
+        }
+        int [] ans = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[src] = 0;
+        PriorityQueue<Triplate> pq  = new PriorityQueue<>();  //min heap;
+        pq.add(new Triplate(src,0,0));
+        while(pq.size()>0){
+            Triplate top = pq.remove();
+            int node = top.node;
+            int cost = top.cost;
+            int stops = top.stops;
+            
+            if(stops == k+1) continue;
+            for (Pair p : adj.get(node)){
+                int total_cost = cost +p.cost;
+                if(ans[p.node ]>total_cost) {
+                    ans[p.node ] = total_cost;
+                    pq.add(new Triplate(p.node,total_cost,stops+1));
+                }
+            }
+        }
+        if(ans[dst] == Integer.MAX_VALUE) return -1;
+        return ans[dst];
+    }
+}
