cc5b573832c3a1005147da81f7d3f5084608dec1
niteshkumar-11n <nitteshkumar12570@gmail.com>
2026-09-14 12:09:50 +0530

    Time: 23 ms (0.83%) | Memory: 45.3 MB (13.39%) - LeetSync

--- 223-rectangle-area/rectangle-area.java
+++ 223-rectangle-area/rectangle-area.java
@@ -1,24 +1,24 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        boolean flag1 = false;
-        boolean flag2= false;
-
-        if(ay1 < by2 && by1 < ay2) flag1 = true;
-        if(ax1 < bx2 && bx1 <ax2) flag2 = true;
-        int x_st = Math.max(ax1,bx1);
-        int x_end = Math.min(ax2,bx2);
-        int x_len = x_end-x_st;
-        int y_st = Math.max(ay1,by1);
-        int y_end = Math.min(ay2,by2);
-        int y_len = y_end - y_st;
-        int overlap_area = x_len * y_len;
         int org_area_1 = (ax2-ax1)*(ay2-ay1);
         int org_area_2 = (bx2-bx1)*(by2-by1);
+
+        int x_st = Math.max(ax1,bx1);
+        int x_end = Math.min(ax2,bx2);
+
+        
+        int y_st = Math.max(ay1,by1);
+        int y_end = Math.min(ay2,by2);
+        int x_len = Math.max(0,(x_end-x_st));
+        int y_len = Math.max(0,(y_end - y_st));
+        
+        int overlap_area = x_len * y_len;
+        
         System.out.println(org_area_1);
         System.out.println(org_area_2);
         System.out.println(overlap_area);
-        if((flag1 && flag2) == true )   return (org_area_1+org_area_2) - overlap_area;
-        else return org_area_1+org_area_2;
+        return (org_area_1+org_area_2) - overlap_area;
+        
         
     }
 }
